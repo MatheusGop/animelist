f98b12f79c3e142f1c3e2e7f0215c39a02b48b0f
MatheusGop <mgp230397@gmail.com>
2019-10-09 21:08:34 -0300

    update html and css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $('.sidenav').sidenav();
+
     $(".card-anime").hover(function () {
         var idC = $(this).data("id");
-        $('.banner-image').each(function() {
+        $('.banner-image').each(function () {
             var id = $(this).data("id");
             if (idC == id) {
                 $(this).css('opacity', '0.1')
